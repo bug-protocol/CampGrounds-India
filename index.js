72a82b21b94f56535f1581903a4841cf3218c298
bug-protocol <ashutoshm9918@gmail.com>
2025-03-12 22:27:07 +0530

    Adding styles using ejs template

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./model/campground');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const Campground = require('./model/campground');
 // Connecting to the database
 mongoose.connect('mongodb://localhost:27017/Grounds-Database');
 
@@ -20,6 +21,7 @@ db.once('open',()=>{
 })
 const app = express();
 
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
 // Body Parser 
