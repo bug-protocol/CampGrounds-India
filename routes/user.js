1df793fa23e76ab7158ac8541043f80d46d3fbb4
bug-protocol <ashutoshm9918@gmail.com>
2025-04-07 23:42:15 +0530

    Add register

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,17 @@ router.get('/register',(req,res)=>{
     res.render('users/register.ejs');
 })
 router.post('/register',async(req,res)=>{
-    res.send(req.body);
+    try{
+        const{username,email,password} = req.body;
+    const user = new User ({email,username});
+    const registeredUser = await User.register(user,password);
+    // console.log(registeredUser);
+    req.flash('Welcome to CampGrounds');
+    res.redirect('/campgrounds');
+    }
+    catch(e){
+        req.flash('error',e.message);
+        res.redirect('/register');
+    }
 })
 module.exports = router;
